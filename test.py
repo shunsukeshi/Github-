98f8a9d0954b9b28e5893e7febe9bd02c14adb0d
S.Shiomi <83161736+shunsukeshi@users.noreply.github.com>
2023-01-31 20:09:30 +0900

    initial commit
    練習です。

---
+++ test.py
@@ -0,0 +1,20 @@
+# print("Hello World!")
+# 
+# print("Hello World!")
+# コメント
+# print("Hello World!")
+
+# class Card:
+    # def __init__(self,date,user_name):
+        # self.date = date
+        # self.user_name = user_name
+    # def message(self):
+        # return "この投稿は"+self.user_name+"さんが"+self.date+"に投稿しました。"
+# date_a = "2020-01-01"
+# user_name_a = "Taro"
+# 
+# card_a = Card(date_a,user_name_a)
+# print(card_a.message())
+
+import math
+print(math.pi)
